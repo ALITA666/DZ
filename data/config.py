f039817aa1082dccdd7a4255035f7e74fa88bc22
ALITA <121035901+ALITA666@users.noreply.github.com>
2023-06-07 20:55:42 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -3,8 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class Tokens:
-    user: str = 'vk1.a.uzZ3YeokXabYndfSOhUoDGRDX2X0FH6w542lHi6_6IIFbxqQ20VIBx6MKUQaT6qof5tlSwnCcJKFSwK_PtCXW_iWW50eGgc7kKSbMnqzyZe1VZlN8GnJ15_rnPV5-MfV5t5ogfhY1812mePmjAPCXt2MvcUxeSH_OK-jlOM0yBsfs4d-knLWz_eJskPqnqODnzSDDXGUVLUrXezAb255VA'
-    group: str = 'vk1.a.bhIF7gg1HFJQHy_mWOVTudihKtc0K0RYWUmgmcMtvWg2aVQIapjYulaCN7BvycYxRgHxIHyxvXEkCYx9zvQKI4zhjlTZhlJzN2ox0rWVefp3coatr_VdSLHO4ht4My6CZzAIfy2UaM0rafsll8CQSM8gCbG3j4VnLcyKKpajymnYdLsuczRLcWa-ZukQrfxPys8chhJbsUqs5pXiRgMNWg'
+    user: str = '1'
+    group: str = '2'
 
 
 @dataclass
